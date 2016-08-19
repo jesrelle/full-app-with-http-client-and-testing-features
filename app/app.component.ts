530d6d92757fd14fe32d2d5cbca92e1e387ff73f
jemae092312 <jemae092312>
2016-08-19 18:36:06 +0800

    all working update to complete the task to a full app

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'my-app',
+    template: `<h1 align="center">My First Angular 2 App</h1>
+        
+    <h1>hey</h1>
+    `
+})
+export class AppComponent { }
